f847703cd8c411ab2df4410a30c07aad5e8e2e1c
박경두 <ruden@baggyeongduui-iMac.local>
2017-07-16 23:49:46 +0900

    add route with route

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,12 @@ import queryString from 'query-string';
 
 const About = ({location, match}) => {
   const query = queryString.parse(location.search);
-  console.log(query);
-  
+  const detail = query.detail === 'true';
+
   return (
     <div>
       About {match.params.name}
+      {detail && 'detail: blablabla'}
     </div>
   )
 }
