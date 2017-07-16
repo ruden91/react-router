97bb6edaf6f134204e0ecb16b5cbd565985e51aa
박경두 <ruden@baggyeongduui-iMac.local>
2017-07-16 23:12:18 +0900

    add yarn package's queryString

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const About = ({match}) => {
+const About = ({location, match}) => {
+  const query = queryString.parse(location.search);
+  console.log(query);
+  
   return (
     <div>
       About {match.params.name}
